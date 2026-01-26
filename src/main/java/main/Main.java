7127321002c8602fe95458e0e3b3c77080415a27
Katharina <katharina.ensslen@outlook.de>
2026-01-26 10:55:51 +0100

    npc und enemy in main importieren

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,26 +2,22 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
-
 package main;
 
+import assets.actors.NPC;
+import assets.actors.Enemy;
+
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello Remote World!");
-        
-        
+
         // hier werden ausgeteilt
-        
-        
-        
-        
         // hier werden zwilling verglichen...
     }
 
-
-
- public Enemy createEnemy(NPC npc)
-    {Enemy e = new Enemy(npc.getX(), npc.getY(), npc.getImg(), npc.getName(), 10, 20);
-        return(e);
+    public Enemy createEnemy(NPC npc) {
+        Enemy e = new Enemy(npc.getX(), npc.getY(), npc.getImg(), npc.getName(), 10, 20);
+        return (e);
     }
 }
